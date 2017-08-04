d2b882bed1c9d43a67a45ed2a9ba51277902be6e
Unknown <hazwiesyazwan@gmail.com>
2017-08-04 17:46:15 +0800

    Mentor Form editing starts

--- app/Http/Controllers/MentorEvaluationFormController.php
+++ app/Http/Controllers/MentorEvaluationFormController.php
@@ -3,75 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\RoleServices as Roles;
+use App\services\LecturerFormServices as LForm;
 
 class MentorEvaluationFormController extends Controller
 {
-    public function index()
+    public function __construct(Roles $role, LForm $lForm)
     {
-        return 'haa dah sampai';
+        $this->middleware('auth');
+        $this->role = $role;
+        $this->lForm = $lForm;
+        $this->title = "Dashboard > Mentor Evaluation";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function index()
+    {
+        $data = $this->role->getRole();
+        $data['title'] = $this->title;
+
+        return view('EvaluationForm.Lecturers.EvaluationForm')->with('data', $data);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
---
+++ resources/views/EvaluationForm/Lecturers/EvaluationForm.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content-app')
+
+<div class="box box-info">
+	<div class="box-header with-border">
+		<h1>Mentor Report</h1>
+	</div>
+</div>
+
+<h2 class="page-header">A. INFORMATION</h2>
+
+<div class="col-md-6">
+	<div class="box box-primary">
+		<div class="box-header with-border">
+			<h3 class="box-title">MENTEE</h3>
+		</div>
+	</div>
+</div>
+
+<div class="col-md-6">
+	<div class="box box-primary">
+		<div class="box-header with-border">
+			<h3 class="box-title">MENTOR</h3>
+		</div>
+	</div>
+</div>
+
+<h2 class="page-header">B. MENTOR-MENTEE SESSION</h2>
+
+<div class="box box-primary">
+	<div class="box-header with-border">
+		
+	</div>
+</div>
+
+@endsection
